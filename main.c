27c30d27108089bc1570653640ddafecbe9d7f85
Mert Gunduz <merttgg@gmail.com>
2021-10-18 13:07:19 +0300

    Variables & Arrays

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int passwordLength;
+    char includeSymbols;
+    char includeNumbers;
+    char includeLowerCase;
+    char includeUpperCase;
+
+    char Symbols_Array[] = {'!', '£', '^', '+', '$', '#', '%', '&', '[', ']', '{', '}', '(', ')', '?', '-', '*'};
+    char Numbers_Array[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char LowerCase_Array[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char UpperCase_Array[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    printf("|------|\n");
+    printf("| JACO |\n");
+    printf("|------|\n\n");
+
+    printf("Password Length --> ");
+    scanf("%d", passwordLength);
     return 0;
 }
